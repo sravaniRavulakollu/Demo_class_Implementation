7f2167dcf2d7929edb0a0f1cccf95388fec998cf
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-14 12:00:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Class_Implementation
+{
+    partial class LMS
+    {
+        string name;
+        int SNO;
+        long mobilenum;
+        string email;
+
+        public void Display()
+        void IAsessment.MCQ_Asessment()
+        void IAsessment.QuestionBank()
+        {
+            Console.WriteLine("Returing value from Question Bank defined inside LMS class");
+        }
+
+    }
+    partial class LMS : Placement,IAsessment
+    {
+        public void Message()
+        {
+            Console.WriteLine("This Message is coming from second Implementation of class");
+        }
+    }
+    abstract class Placement
+    {
+        string name_of_company;
+        int No_of_students_placed;
+        DateTime Year_of_placement;
+
+        public void placement_record()
+        {
+            Console.WriteLine("This method will show you complete record of the placement");
+
+        }
+
+    }
+    public interface Asessment
+    {
+        public void MCQ_Asessment();
+        public void QuestionBank();
+    }
+}
+
+
+
+   
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo_Class_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LMS sravs = new LMS();
+            sravs.Display();
+            sravs.Message();
+            sravs.placement_record();
+           
+     
+            IAsessment Delhi = sravs;
+            Delhi.MCQ_Asessment();
+            Delhi.QuestionBank();
+
+            //Placement NIT = new Placement();
+
+        }
+    }
+}
